2c2ebb1d1a10337138272ee1978a2f8e752a3b7c
Akshay Kumar <mail.akshay04@gmail.com>
2020-05-01 04:01:53 +0530

    verification email fixed

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,7 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
-import 'ui/sign_in.dart';
-import 'ui/sign_up.dart';
-import 'package:firebase_auth/firebase_auth.dart';
+import 'pages/root_page.dart';
+import 'services/authentication.dart';
 
 import 'ui/web_view.dart';
 
@@ -10,7 +9,7 @@ Future<void> main() async {
   runApp(new MaterialApp(
     home: new SplashScreen(),
     routes: <String, WidgetBuilder>{
-      '/Login': (BuildContext context) => new LoginPage(),
+      '/Login': (BuildContext context) => new RootPage(auth: new Auth()),
       '/Home' : (BuildContext context) => new WebView(),
     },
   ));
@@ -28,15 +27,14 @@ class _SplashScreenState extends State<SplashScreen> {
 
 
   startTime() async {
-    if (await FirebaseAuth.instance.currentUser() != null) {
-      var _duration = new Duration(seconds: 3);
-      return new Timer(_duration, navigationHome);
-      // signed in
-    } else {
+//    if (await FirebaseAuth.instance.currentUser() != null) {
+//      var _duration = new Duration(seconds: 3);
+//      return new Timer(_duration, navigationHome);
+//      // signed in
+//    } else {
       var _duration = new Duration(seconds: 3);
       return new Timer(_duration, navigationLogin);
 
-    }
 
   }
 
@@ -59,7 +57,10 @@ class _SplashScreenState extends State<SplashScreen> {
   Widget build(BuildContext context) {
     return new Scaffold(
       body: new Center(
-        child: new Image.asset('images/sameday.png'),
+        child: Padding(
+          padding: EdgeInsets.fromLTRB(25.0, 25.0, 25.0, 25.0),
+          child: new Image.asset('images/sameday.png'),
+        )
       ),
     );
   }
---
+++ lib/pages/home_page.dart
@@ -0,0 +1,124 @@
+import 'package:flutter/material.dart';
+import '../services/authentication.dart';
+import '../ui/web_view.dart';
+import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
+
+class HomePage extends StatefulWidget {
+  HomePage({Key key, this.auth, this.userId, this.logoutCallback})
+      : super(key: key);
+
+  final BaseAuth auth;
+  final VoidCallback logoutCallback;
+  final String userId;
+
+  @override
+  State<StatefulWidget> createState() => new _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final GlobalKey<FormState> formKey = GlobalKey<FormState>();
+
+  bool _isEmailVerified = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _checkEmailVerification();
+  }
+
+  void _checkEmailVerification() async {
+    _isEmailVerified = await widget.auth.isEmailVerified();
+    if (!_isEmailVerified) {
+      _showVerifyEmailDialog();
+    } else {
+      Navigator.pushAndRemoveUntil(
+          context,
+          MaterialPageRoute(
+              builder: (context) => WebView()
+          ),
+          ModalRoute.withName("/Home")
+      );
+    }
+  }
+
+  void _resentVerifyEmail() {
+    widget.auth.sendEmailVerification();
+    _showVerifyEmailSentDialog();
+  }
+
+  void _showVerifyEmailDialog() {
+    showDialog(
+      barrierDismissible: false,
+      context: context,
+      builder: (BuildContext context) {
+        // return object of type Dialog
+        return AlertDialog(
+          title: new Text("Verify your account"),
+          content: new Text("Please verify account in the link sent to email"),
+          actions: <Widget>[
+            new FlatButton(
+              child: new Text("Resent link"),
+              onPressed: () {
+                Navigator.of(context).pop();
+                _resentVerifyEmail();
+              },
+            ),
+//            new FlatButton(
+//              child: new Text("Dismiss"),
+//              onPressed: () {
+//                Navigator.of(context).pop();
+//              },
+//            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void _showVerifyEmailSentDialog() {
+    showDialog(
+      barrierDismissible: false,
+      context: context,
+      builder: (BuildContext context) {
+        // return object of type Dialog
+        return AlertDialog(
+          title: new Text("Verify your account"),
+          content:
+              new Text("Link to verify account has been sent to your email"),
+          actions: <Widget>[
+//            new FlatButton(
+//              child: new Text("Dismiss"),
+//              onPressed: () {
+//                Navigator.of(context).pop();
+//              },
+//            ),
+          ],
+        );
+      },
+    );
+  }
+
+  signOut() async {
+    try {
+      await widget.auth.signOut();
+      widget.logoutCallback();
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text('Same Day Rush'),
+        actions: <Widget>[
+          new FlatButton(
+              child: new Text('Logout',
+                  style: new TextStyle(fontSize: 17.0, color: Colors.white)),
+              onPressed: signOut)
+        ],
+      ),
+    );
+  }
+}
